6ea81ad71851e340cf9bfa4caa337b421bd319c1
cb0n3y <cb0n3y@gmail.com>
2025-04-18 12:26:48 +0200

    change the __init__ function in SpeedtestResults class to python3 syntax
    Signed-off-by: cb0n3y <cb0n3y@gmail.com>

--- speedtest.py
+++ speedtest.py
@@ -957,7 +957,7 @@ class SpeedtestResults(object):
         self.client = client or {}
 
         self._share = None
-        self.timestamp = '%sZ' % datetime.datetime.now(datetime.UTC).isoformat()
+        self.timestamp = datetime.datetime.now(datetime.UTC).replace(microsecond=0).isoformat().replace('+00:00', 'Z')
         self.bytes_received = 0
         self.bytes_sent = 0
 
